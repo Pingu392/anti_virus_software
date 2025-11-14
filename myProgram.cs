98ef6fa2f6954fe9a6f498e8d63003fa395fd176
Pingu392 <mkphung2004@gmail.com>
2025-11-13 23:19:38 -0500

    Added the main program and configs

---
+++ myProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace Project1
+{
+    internal class myProgram
+    {
+        static void Main(String[] args) {
+            String root = @"C:\Users\swive\Documents\C_Sharp_Dir";
+            String subroot = @"C:\Users\swive\Documents\C_Sharp_Dir\subDir";
+
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+                Console.Write("Directory created!");
+            }
+
+            Directory.Delete(root, true);
+        }
+    }
+}
