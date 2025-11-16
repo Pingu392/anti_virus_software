360100487681b9e29ec242f37e83a464c9d28d43
Billy <mkphung2004@gmail.com>
2025-11-16 14:39:25 -0500

    Updated main program to get directory's file contents and compute SHA256 hashes from them.

--- myProgram.cs
+++ myProgram.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO; 
+using System.IO;
+using System.Security.Cryptography;
 
 namespace Project1
 {
     internal class myProgram
     {
         static void Main(String[] args) {
-            String root = @"C:\Users\swive\Documents\C_Sharp_Dir";
-            String subroot = @"C:\Users\swive\Documents\C_Sharp_Dir\subDir";
+            DirectoryInfo directory = new DirectoryInfo("C:\\Users\\swive\\Documents\\C_Sharp_Dir");
 
-            if (!Directory.Exists(root))
+            try
             {
-                Directory.CreateDirectory(root);
-                Console.Write("Directory created!");
-            }
+                if (!directory.Exists)
+                {
+                    Console.Write("Directory does not exist!");
+                    return;
+                }
 
-            Directory.Delete(root, true);
+                String[] files = Directory.GetFiles(directory.FullName);
+                foreach (var file in files) {
+                    string hash = ComputeFileHash(file.ToString());
+                    Console.WriteLine($"File: {file.ToString()}\nHash: {hash}\n");
+                }
+
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+
+        static string ComputeFileHash(string filePath) {
+            using (var sha256 = SHA256.Create()) {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    byte[] hashBytes = sha256.ComputeHash(stream);
+                    return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+                }
+            }
         }
     }
 }
+//238539696ebfd7845d0f0f56c06746396053f500029845d96e6476105709d2e5
